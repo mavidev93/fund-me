8ec3594a3ba202729b59562a61d6f64913e3c026
unknown <mavidev93@gmail.com>
2022-02-14 11:54:07 +0330

    fund me contract

---
+++ .history/src/App_20220214111555.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Gretter from './artifacts/'
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center">hi I'm abdolreza</h1>
+    </div>
+  );
+}
+
+export default App;
